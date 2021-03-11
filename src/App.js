8094eaca569d87587c9e847918b0dd47128065ed
AlexandrKirichenko <kiri4enkoalexandr@gmail.com>
2021-03-11 21:10:19 +0200

    hw2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Footer from "./components/Footer";
 import './App.css';
+import ReactLogo from '../src/assets/bg1.jpg';
+import ReactLogo2 from '../src/assets/bg2.jpg';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi, maybe sometimes it is worth resting more ;-)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header
+        title="This is new Title"
+        descr="This is perfect description"
+        />
+      <Layout
+        title="L1"
+        descr="This is perfect description"
+        urlBg={ReactLogo}
+        colorBg/>
+      <Layout
+        colorBg="red"
+        title="L2"
+        descr="This is perfect description2"
+       />
+      <Layout
+        title="L3"
+        descr="This is perfect description3"
+        urlBg={ReactLogo2} />
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
